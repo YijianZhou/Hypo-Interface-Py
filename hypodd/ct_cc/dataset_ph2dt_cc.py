cc64c19c46c2dc950cad467fd93b59a4b6f2ecd1
Yijian ZHOU <34815814+YijianZhou@users.noreply.github.com>
2021-06-27 13:28:49 +0800

    Update dataset_ph2dt_cc.py

--- hypodd/ct_cc/dataset_ph2dt_cc.py
+++ hypodd/ct_cc/dataset_ph2dt_cc.py
@@ -32,14 +32,14 @@ ot_min, ot_max = [UTCDateTime(date) for date in cfg.ot_range.split('-')]
 def get_event_list(fpha_temp, event_root):
     # 1. read phase file
     print('reading phase file')
-    event_list = read_fpha_temp(fpha_temp)
-    num_events = len(event_list)
+    event_pick_list = read_fpha_temp(fpha_temp)
+    num_events = len(event_pick_list)
     # 2. get stream paths
     print('getting event data paths:')
-    bad_list = []
-    for i, [evid, event_name, event_loc, pha_dict_pick] in enumerate(event_list): 
+    event_list = []
+    for i, [evid, event_name, event_loc, pha_dict_pick] in enumerate(event_pick_list): 
         if i%2000==0: print('%s events done'%i)
-        if len(pha_dict_pick)<num_sta_thres: bad_list.append(i); continue
+        if len(pha_dict_pick)<num_sta_thres: continue
         # select station by epi-dist
         dtype = [('sta','O'),('tp','O'),('ts','O')]
         picks = np.array([(sta,tp,ts) for sta,[tp,ts] in pha_dict_pick.items()], dtype=dtype)
@@ -51,8 +51,8 @@ def get_event_list(fpha_temp, event_root):
             st_paths = sorted(glob.glob(os.path.join(event_dir, '%s.*'%net_sta)))
             if len(st_paths)!=3: continue
             pha_dict[net_sta] = [st_paths, tp, ts]
-        event_list[i] = [evid, event_loc, pha_dict]
-    return np.delete(event_list, bad_list)
+        event_list.append([evid, event_loc, pha_dict])
+    return event_list
 
 
 # read event data as data & temp
