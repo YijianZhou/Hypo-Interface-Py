faf5f65dbd368adf2c70a71c2bcaff7ac95c55d4
Yijian ZHOU <34815814+YijianZhou@users.noreply.github.com>
2022-05-04 12:19:49 -0700

    Update config.py

--- hypodd/ct/config.py
+++ hypodd/ct/config.py
@@ -7,16 +7,18 @@ import numpy as np
 class Config(object):
   def __init__(self):
 
+    # i/o paths
     self.hypo_root = '/home/zhouyj/bin'
     self.ctlg_code = 'eg_ct'
     self.fsta = 'input/station_eg.csv'
     self.fpha = 'input/eg_hyp_full.pha'
-    self.dep_corr = 5 # avoid air quake
+    # run ph2dt & hypoDD 
+    self.dep_corr = 5 # avoid air quake, modify velo_mod accordingly
     self.ot_range = '20190704-20190710'
     self.lat_range = [35.45,36.05]
     self.lon_range = [-117.8,-117.25]
-    self.num_grids = [1,1] # x,y (lon, lat)
-    self.xy_pad = [0.056,0.046] # degree
-    self.num_workers = 1
-    self.keep_grids = False
+    self.num_grids = [1,1] # reloc by grid, x,y (lon, lat)
+    self.xy_pad = [0.056,0.046] # additional events in each grid, degree
+    self.num_workers = 1 
+    self.keep_grids = False # whether keep additional outputs in each grid
 
